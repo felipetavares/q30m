ef1e576a73074517c98288a7d5cdb30b51564bfe
Felipe Tavares <felipe.oltavares@gmail.com>
2023-03-13 00:24:46 +1100

    improve comments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 mod tensor;
 mod register;
 
@@ -6,11 +5,14 @@ use register::QuantumRegister;
 
 fn main() {
     // 1. Prepare a state that's either |00> or |11>: either bit can be 0 or 1 but after a
-    //    measurement, the other bit will be have the same value.
+    //    measurement, the other bit will have the same value.
+    //
+    //    Note the use of macros to describe standard kets and also the tensor product represented
+    //    as `.prod()`.
     let entangled_state = &(ket![|0>].prod(&ket![|0>]) + ket![|1>].prod(&ket![|1>])) / 2f64.sqrt();
     let mut register = QuantumRegister::new(entangled_state, 2);
 
-    // 2. Measure both bits
+    // 2. Measure both qubits
     dbg!(register.measure(0));
     dbg!(register.measure(1));
 }
